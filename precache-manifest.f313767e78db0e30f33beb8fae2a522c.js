56944fd22246ebf40132861f5cd4414262bb718e
LeeJaeHun <LeeJaeHun@users.noreply.github.com>
2020-10-15 17:25:23 +0900

    Updates

---
+++ precache-manifest.f313767e78db0e30f33beb8fae2a522c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc184c7688779375cc72e78673a1f30e",
+    "url": "/simple-todo/index.html"
+  },
+  {
+    "revision": "5579cb3789a308bce94d",
+    "url": "/simple-todo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2c002f99a99023c91ab0",
+    "url": "/simple-todo/static/js/2.a065bf55.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/simple-todo/static/js/2.a065bf55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5579cb3789a308bce94d",
+    "url": "/simple-todo/static/js/main.763e988f.chunk.js"
+  },
+  {
+    "revision": "a9b828f3067ebb10d15b",
+    "url": "/simple-todo/static/js/runtime-main.b107b839.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/simple-todo/static/media/logo.5d5d9eef.svg"
+  }
+]);
